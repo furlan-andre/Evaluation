8ccfbb7fdad95a5e52673d7dd8109189f533cc65
Andre Furlan <furlan.andre91@gmail.com>
2025-04-20 20:58:42 -0400

    feat: Added SaleConfiguration

---
+++ src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleConfiguration.cs
@@ -0,0 +1,24 @@
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping.Sales;
+
+public class SaleConfiguration : IEntityTypeConfiguration<Sale>
+{
+    public void Configure(EntityTypeBuilder<Sale> builder)
+    {
+        builder.ToTable("Sales");
+
+        builder.HasKey(s => s.Id);
+        builder.Property(s => s.Id).HasColumnType("uuid").HasDefaultValueSql("gen_random_uuid()");
+        
+        builder.Property(s => s.SaleNumber).IsRequired();
+        builder.Property(s => s.SaleDate).HasColumnType("datetime").IsRequired();
+        builder.Property(s => s.CustomerId).IsRequired();
+        builder.Property(s => s.BranchId).IsRequired();
+        builder.Property(s => s.BranchName).IsRequired();
+        builder.Property(s => s.TotalAmount).IsRequired().HasColumnType("decimal(18,2)");
+        builder.Property(s => s.Active).IsRequired();
+    }
+}
