f4b8f0ea01bcbb7c9f00cde8eb0f39c0d4ecffea
Andre Furlan <furlan.andre91@gmail.com>
2025-04-20 22:34:20 -0400

    fix: setting defaults in Sale entity

--- src/Ambev.DeveloperEvaluation.Domain/Entities/Sale.cs
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/Sale.cs
@@ -36,17 +36,17 @@ public class Sale : BaseEntity
     /// Gets the name of branch
     /// It is a name that identify the branch that made the sale
     /// </summary>
-    public string BranchName { get; set; }
+    public string BranchName { get; set; } = string.Empty;
     
     /// <summary>
     /// Gets the total amount of the sale
     /// It is a number that identify the total amount of the sale
     /// </summary>
     public decimal TotalAmount { get; set; }
-    
+
     /// <summary>
     /// Gets the representation of valid state for a sale
     /// It is a boolean value that identify the sale is active or inactive (cancelled)
     /// </summary>
-    public bool Active { get; set; }
+    public bool Active { get; set; } = true;
 }
