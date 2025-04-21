82cfc06e1339acd18acae7caf20688bc289833e3
Andre Furlan <furlan.andre91@gmail.com>
2025-04-21 18:00:10 -0400

    feat: Added handler, response, request, validators, commands for Delete sales feature, Added end point for delete sales too.

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/DeleteSales/DeleteSaleRequestValidator.cs
@@ -0,0 +1,19 @@
+using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.DeleteSales;
+
+/// <summary>
+/// Validator for DeleteSaleRequest
+/// </summary>
+public class DeleteSaleRequestValidator : AbstractValidator<DeleteSaleRequest>
+{
+    /// <summary>
+    /// Initializes validation rules for DeleteSaleRequest
+    /// </summary>
+    public DeleteSaleRequestValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty()
+            .WithMessage("User ID is required");   
+    }
+}
